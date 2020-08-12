af74522854fd9a30c8a18a32360923fd0e7796a8
xuxiaozhou <759811542@qq.com>
2020-08-12 21:04:09 +0800

    111

--- src/lib/danmu/index.js
+++ src/lib/danmu/index.js
@@ -1,26 +1,26 @@
-'use strict'
+'use strict';
 
-const Player = require('./player')
-const player = new Player()
+const Player = require('./player');
+const player = new Player();
 
-function send (param) {
-  player.parseDanmus([param])
-  player.controlDanmu('update')
+function send(param) {
+  player.parseDanmus([param]);
+  player.controlDanmu('update');
 }
 
-function init (config, object) {
-  window.config = config
-  player.setup(object, 'canvas-danmu')
-  player.controlDanmu('play')
-  window.console.log('弹幕初始化完成！')
+function init(config, object) {
+  window.config = config;
+  player.setup(object, 'canvas-danmu');
+  player.controlDanmu('play');
+  window.console.log('弹幕初始化完成！');
 }
 
 module.exports = {
   init: init,
   send: send,
-  example: function () {
-    let i = 1
-    let id = 10000
+  example() {
+    let i = 1;
+    let id = 10000;
     setInterval(function () {
       send({
         text: '[IMG WIDTH=24]danmu-24.png[/IMG]测试[IMG WIDTH=24]danmu-24.png[/IMG]Hello World[IMG WIDTH=24]danmu-24.png[/IMG]',
@@ -29,29 +29,31 @@ module.exports = {
         textStyle: 'normal bold ' + i + 'em 微软雅黑',
         height: i * 10,
         id: id
-      })
-      i++
-      if (i > 5) i = 1
-    }, 10)
+      });
+      i++;
+      if (i > 5) {
+        i = 1;
+      }
+    }, 10);
   },
-  resize: function (object) {
-    let w = object.offsetWidth // 控件的宽
-    let h = object.offsetHeight // 控件的高
+  resize(object) {
+    let w = object.offsetWidth; // 控件的宽
+    let h = object.offsetHeight; // 控件的高
 
-    player.frame.resize(w, h)
-    player.canvas.width = w
-    player.canvas.height = h
+    player.frame.resize(w, h);
+    player.canvas.width = w;
+    player.canvas.height = h;
   },
-  stop: function () {
-    player.frame.stopDanmu()
+  stop() {
+    player.frame.stopDanmu();
   },
-  start: function () {
-    player.frame.restartDanmu()
+  start() {
+    player.frame.restartDanmu();
   },
-  clear: function () {
-    player.frame.clearDanmu()
+  clear() {
+    player.frame.clearDanmu();
   }
-}
+};
 
 // 给window添加私货方便调试
-window.danmuControl = module.exports
+window.danmuControl = module.exports;
